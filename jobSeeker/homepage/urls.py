baa82428166de5f062951643fd5448a0150bc712
DhruvVishalGupta <113868257+DhruvVishalGupta@users.noreply.github.com>
2025-11-23 12:55:16 -0500

    Fixed issue with exporting

--- jobSeeker/homepage/urls.py
+++ jobSeeker/homepage/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='homepage.index'),
     path('about/', views.about, name='homepage.about'),
-    path('admin/export/', views.admin_export, name='homepage.admin_export'),
-    path('admin/export/<str:data_type>/', views.export_csv, name='homepage.export_csv'),
+    path('data-export/', views.admin_export, name='homepage.admin_export'),
+    path('data-export/<str:data_type>/', views.export_csv, name='homepage.export_csv'),
 ] 
